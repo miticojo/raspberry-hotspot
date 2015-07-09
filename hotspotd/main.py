abe4743456b00f98ba549f422225617765092651
miticojo <miticojo@gmail.com>
2015-07-09 08:09:41 +0200

    iptable mark added to exclude portal redirect from Trusted clients

--- hotspotd/main.py
+++ hotspotd/main.py
@@ -18,21 +18,19 @@ class Manager():
         os.system("iptables -P INPUT DROP")
         os.system("iptables -P FORWARD DROP")
         os.system("iptables -P OUTPUT ACCEPT")
+
         # Allow loopback access
         os.system("iptables -A INPUT -i lo -j ACCEPT")
+
         # Ping from outside to inside
         os.system("iptables -A INPUT -p icmp -j ACCEPT")
-        # Accept DNS connection from external
-        #os.system("iptables -A INPUT -p udp -i eth0 --sport 53 -j ACCEPT")
-        # Accept DNS connection from external
-        #os.system("iptables -A INPUT -p udp -i eth0 --sport 123 -j ACCEPT")
+
         # Accept Safe Port from web
         os.system("iptables -A INPUT -i eth0 -j ACCEPT")
 
         # Allow outbound DNS from wlan0 to local
         os.system("iptables -A INPUT -p udp -i wlan0 -d 10.0.0.1/32 --dport 53 -j ACCEPT")
-        # Allow incoming SSH only from a sepcific network
-        #os.system("iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT")
+
         # Prevent DoS attack
         os.system("iptables -A INPUT -p tcp --dport 80 -m limit --limit 25/minute --limit-burst 100 -j ACCEPT")
 
@@ -46,21 +44,16 @@ class Manager():
         os.system("iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE")
 
         os.system("iptables -A FORWARD -i eth0 -j ACCEPT")
-        #os.system("iptables -A FORWARD -i wlan0 -j ACCEPT")
-
+        #os.system("iptables -A FORWARD -i wlan0 -m mark --mark 2 -j ACCEPT")
 
         # Create chan for Trusted client
         os.system("iptables -N Trusted")
         os.system("iptables -A FORWARD -j Trusted")
-        #os.system("iptables -A Trusted -m mac --mac-source 74:81:14:38:e1:4a -j ACCEPT")
-
-
-        #os.system("iptables -N TrustedNat -t nat")
-        #os.system("iptables -t nat -A PREROUTING -j TrustedNat")
+        os.system("iptables -t nat -N Trusted")
+        os.system("iptables -t nat -A PREROUTING -j Trusted")
 
         # Redirect ALL web traffic to captive portal
-        os.system("iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j DNAT --to-destination 10.0.0.1:5000")
-
+        os.system("iptables -t nat -A PREROUTING -i wlan0 -m mark ! --mark 2 -p tcp --dport 80 -j DNAT --to-destination 10.0.0.1:5000")
 
         # Enable logging
         os.system("iptables -N LOGGING")
@@ -106,7 +99,13 @@ class Manager():
         def register():
             if request.form['submit'] == 'Register':
                 mac = self.getMACAddress(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
+                # Remove previous rule
+                os.system("iptables -t nat -D Trusted -m mac --mac-source %s -j MARK" % mac )
+                os.system("iptables -D Trusted -m mac --mac-source %s -j ACCEPT" % mac )
+                # Add new rule
+                os.system("iptables -t nat -A Trusted -m mac --mac-source %s -j MARK" % mac )
                 os.system("iptables -A Trusted -m mac --mac-source %s -j ACCEPT" % mac )
+
                 return render_template('index.html', register=True)
 
         self.webapp.run(host='10.0.0.1', port=5000, threaded=True, debug=debug)
